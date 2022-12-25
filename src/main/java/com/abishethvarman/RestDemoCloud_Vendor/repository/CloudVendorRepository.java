e66917f6dee289e7ff6bf615a0ed53ef14790541
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2022-12-26 02:50:59 +0530

    CloudVendorRepository created and extended from JpaRepository

---
+++ src/main/java/com/abishethvarman/RestDemoCloud_Vendor/repository/CloudVendorRepository.java
@@ -0,0 +1,7 @@
+package com.abishethvarman.RestDemoCloud_Vendor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.abishethvarman.RestDemoCloud_Vendor.model.CloudVendor;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String>{
+}
