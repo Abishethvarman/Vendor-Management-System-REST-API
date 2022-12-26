a05b071369aa1999e02b3335440451fe5071d853
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2022-12-27 01:21:44 +0530

    CloudVendorRepositoryTest created with 2 tests

---
+++ src/test/java/com/abishethvarman/RestDemoCloud_Vendor/repository/CloudVendorRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.abishethvarman.RestDemoCloud_Vendor.repository;
+
+
+import com.abishethvarman.RestDemoCloud_Vendor.model.CloudVendor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class CloudVendorRepositoryTest {
+    @Autowired
+    private CloudVendorRepository cloudVendorRepository;
+    CloudVendor cloudVendor;
+
+    @BeforeEach
+    void setUp() {
+        cloudVendor = new CloudVendor("1", "Amazon", "2455", "aser");
+        cloudVendorRepository.save(cloudVendor);
+    }
+
+    @AfterEach
+    void tearDown() {
+        cloudVendor = null;
+        cloudVendorRepository.deleteAll();
+    }
+
+    //Test case - SUCCESS
+    @Test
+    void Should_Return_Find_By_VendorName() {
+        List<CloudVendor> cloudVendorList = cloudVendorRepository.findByVendorName("Amazon");
+        assertThat(cloudVendorList.get(0).getVendorId()).isEqualTo(cloudVendor.getVendorId());
+        assertThat(cloudVendorList.get(0).getVendorAddress()).isEqualTo(cloudVendor.getVendorAddress());
+    }
+
+    //Test case - FAILURE
+    @Test
+    void Should_Not_Return_Find_By_VendorName_Does_Not_Exist() {
+        List<CloudVendor> cloudVendorList = cloudVendorRepository.findByVendorName("GCP");
+        assertThat(cloudVendorList.isEmpty()).isTrue();
+        assertThat(!cloudVendorList.isEmpty()).isFalse();
+    }
+}
