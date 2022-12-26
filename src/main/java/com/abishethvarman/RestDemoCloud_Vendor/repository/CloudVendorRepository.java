3168c6d68ca8d2b8c6360556cd21aa16fd415f54
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2022-12-27 01:20:14 +0530

    findByVendorName function created

--- src/main/java/com/abishethvarman/RestDemoCloud_Vendor/repository/CloudVendorRepository.java
+++ src/main/java/com/abishethvarman/RestDemoCloud_Vendor/repository/CloudVendorRepository.java
@@ -3,5 +3,8 @@ package com.abishethvarman.RestDemoCloud_Vendor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.abishethvarman.RestDemoCloud_Vendor.model.CloudVendor;
 
+import java.util.List;
+
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String>{
+    List<CloudVendor> findByVendorName(String vendorName);
 }
